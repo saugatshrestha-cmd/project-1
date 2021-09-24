2ad1f3f5dcb3241d0e076f84c7b436bd8c5f652f
Saugat Shrestha <55283691+saugatshrestha-cmd@users.noreply.github.com>
2021-09-24 14:25:02 +0545

    Add files via upload

--- handler/setting.php
+++ handler/setting.php
@@ -1,12 +1,23 @@
 <?php 
-
 $epay_url = "https://uat.esewa.com.np/epay/main";
-$pid = "ee2c3ca1-696b-4cc5-a6be-2c40d929d456";
 $successurl = 'http://localhost/project/handler/success.php';
 $failedurl = 'http://localhost/project/handler/failed.php';
 $merchant_code = "epay_payment"; 
 $fraudcheck_url = "https://uat.esewa.com.np/epay/transrec";
 
-$total = 3000;
 
+$host="localhost";
+$user="root";
+$password="";
+$dbname="jutta";
+
+$connect=mysqli_connect($host,$user,$password,$dbname);
+$sql="Select total from orders order by id DESC limit 1";
+$result=$connect->query($sql);
+$final=$result->fetch_assoc();
+$total=$final['total'];
+$sql2="Select order_id from order_details order by id DESC limit 1";
+$result=$connect->query($sql2);
+$final=$result->fetch_assoc();
+$pid=$final['order_id'];
 ?>
