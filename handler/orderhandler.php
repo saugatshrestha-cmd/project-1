81cd4da117e74a0be36c9566190558c261179638
Saugat Shrestha <55283691+saugatshrestha-cmd@users.noreply.github.com>
2021-09-24 22:00:05 +0545

    Add files via upload

--- handler/orderhandler.php
+++ handler/orderhandler.php
@@ -1,22 +1,16 @@
 <?php
 session_start();
 include('../partials/connect.php');
-$total=$_POST['total'];
-$phone=$_POST['phone'];
-$address=$_POST['address'];
-$customerid=$_SESSION['customerid'];
-$payment=$_POST['payment'];
-$sql="INSERT INTO orders(customer_id, address, phone, total, pay_method) VALUES('$customerid','$address','$phone','$total','$payment')";
-$connect->query($sql);
-$sql2="Select id from orders order by id DESC limit 1";
-$result=$connect->query($sql2);
-$final=$result->fetch_assoc();
-$orderid=$final['id'];
 foreach($_SESSION['cart'] as $key => $value){
+    $total=$_POST['total'];
+    $phone=$_POST['phone'];
+    $address=$_POST['address'];
+    $customerid=$_SESSION['customerid'];
+    $payment=$_POST['payment'];
     $proid=$value['item_id'];
     $quantity=$value['quantity'];
-    $sql3="INSERT INTO order_details(order_id,product_id,quantity) VALUES('$orderid','$proid','$quantity')";
-    $connect->query($sql3);
+    $sql="INSERT INTO orders(product_id, customer_id,address, phone, quantity,total, pay_method) VALUES('$proid','$customerid','$address','$phone','$quantity','$total','$payment')";
+    $connect->query($sql);
 }
 if($payment=="esewa"){
     $_SESSION['total']=$total;
