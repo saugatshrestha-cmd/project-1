8f7e29f95e3c548600c16b920e5da952db794c36
Saugat Shrestha <55283691+saugatshrestha-cmd@users.noreply.github.com>
2021-09-28 11:14:52 +0545

    Add files via upload

--- jutta.sql
+++ jutta.sql
@@ -1,226 +1,62 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 18, 2021 at 07:11 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `jutta`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
 
 CREATE TABLE `admins` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `username` varchar(30) DEFAULT NULL,
   `password` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `admins`
---
+)
 
 INSERT INTO `admins` (`id`, `username`, `password`) VALUES
 (1, 'something@gmail.com', '12345');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `name` varchar(30) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
+)
 
 INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Men', '2021-05-25 03:13:31', '2021-05-25 03:13:31'),
 (2, 'Women', '2021-05-25 03:13:37', '2021-05-25 03:13:37');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `customers`
---
-
 CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
---
--- Dumping data for table `customers`
---
 
 INSERT INTO `customers` (`id`, `username`, `password`, `created_at`, `update_at`) VALUES
 (4, 'something@gmail.com', '123', '2021-09-03 19:40:30', '2021-09-03 19:40:30');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `product_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `address` varchar(50) NOT NULL,
   `phone` varchar(30) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `total` int(20) NOT NULL,
   `pay_method` varchar(20) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_details`
---
-
-CREATE TABLE `order_details` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
+)
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `name` varchar(60) NOT NULL,
   `price` int(11) NOT NULL,
-  `picture` varchar(50) NOT NULL,
+  `picture` varchar(90) NOT NULL,
   `category_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
---
--- Dumping data for table `products`
---
 
 INSERT INTO `products` (`id`, `name`, `price`, `picture`, `category_id`, `created_at`, `updated_at`) VALUES
-(1, 'Nike Air', 3000, 'uploads/air-presto.png', 1, '2021-05-25 03:13:52', '2021-05-25 03:13:52'),
-(4, 'Nike', 2000, 'uploads/air-max-plus.jfif.png', 1, '2021-08-10 08:52:14', '2021-08-10 08:52:14'),
-(5, 'Nike', 2000, 'uploads/blazer-mid-777.jfif.png', 2, '2021-08-10 08:52:33', '2021-08-10 08:52:33'),
-(6, 'Nike', 3000, 'uploads/1.png', 1, '2021-08-10 09:26:49', '2021-08-10 09:26:49');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admins`
---
-ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admins`
---
-ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
-
---
--- AUTO_INCREMENT for table `order_details`
---
-ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(7, 'Nike Air Max', 2000, 'uploads/1.png', 1, '2021-09-24 07:30:27', '2021-09-24 07:30:27'),
+(9, 'Nike Air Presto', 3000, 'uploads/air-presto.png', 1, '2021-09-24 07:36:12', '2021-09-24 07:36:12'),
+(13, 'Nike Blazer Mid 777', 2500, 'uploads/blazer-mid-777.jfif.png', 2, '2021-09-24 07:47:18', '2021-09-24 07:47:18');
